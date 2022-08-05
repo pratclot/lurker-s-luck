18155176b74d657030d2547db803957f200b1163
chief <pratclot@live.ru>
2022-08-05 09:30:33 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Lurker's luck
+
+This simple Firefox extension is intended for anonymous `reddit.com` visitors.
+
+There is an amazing `libredd.it` project that once again gives us freedom to read what people share (and what Google conveniently indexed) without giving our personal data, watching unimaginable amounts of ads, terrible web UX and all these things that you hate.
+
+Please note, that this extension is designed specifically for `reddit.com` and people who prefer not to register there. The advantage over a much better `Redirector` extension is that you do not need to give read-write permissions for **ALL** pages, which I find much, much more acceptable.
---
+++ lurkersluck.js
@@ -0,0 +1,55 @@
+const TITLE_APPLY = "Lurker's luck";
+const APPLICABLE_PROTOCOLS = ["http:", "https:"];
+const PATH_ICON = "icons/icon.png";
+
+/**
+ * The code is taken from [apply-css] example over on [webextensions-examples] repo.
+ */
+
+/*
+Returns true only if the URL's protocol is in APPLICABLE_PROTOCOLS.
+Argument url must be a valid URL string.
+*/
+function protocolIsApplicable(url) {
+    const protocol = (new URL(url)).protocol;
+    return APPLICABLE_PROTOCOLS.includes(protocol);
+}
+/*
+Initialize the page action: set icon and title, then show.
+Only operates on tabs whose URL's protocol is applicable.
+*/
+function initializePageAction(tab) {
+    if (protocolIsApplicable(tab.url)) {
+        browser.pageAction.setIcon({tabId: tab.id, path: PATH_ICON});
+        browser.pageAction.setTitle({tabId: tab.id, title: TITLE_APPLY});
+        browser.pageAction.show(tab.id);
+    }
+}
+
+/*
+When first loaded, initialize the page action for all tabs.
+*/
+var gettingAllTabs = browser.tabs.query({});
+gettingAllTabs.then((tabs) => {
+    for (let tab of tabs) {
+        initializePageAction(tab);
+    }
+});
+
+/*
+Each time a tab is updated, reset the page action for that tab.
+*/
+browser.tabs.onUpdated.addListener((id, changeInfo, tab) => {
+    initializePageAction(tab);
+});
+
+function redirect(requestDetails) {
+    console.debug("Redirecting from: " + requestDetails.url);
+
+    let targetUrl = requestDetails.url.replace("www.reddit.com", "libredd.it")
+    console.debug("Redirecting to: " + targetUrl);
+
+    browser.tabs.update({url: targetUrl});
+}
+
+browser.pageAction.onClicked.addListener(redirect);
